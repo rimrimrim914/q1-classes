bd0b555e68bbe15db7e718bbc542ac0d7aa1d707
rimrimrim914 <51864390+rimrimrim914@users.noreply.github.com>
2019-10-06 19:07:29 +0500

    Add files via upload

---
+++ Euclidean distance.py
@@ -0,0 +1,29 @@
+## 10. Euclidean distance
+### write a Python program to compute the distance between the points (x1, y1) and (x2, y2).
+#### Program Console Sample 1:
+###### Enter Co-ordinate for x1: 2
+###### Enter Co-ordinate for x2: 4
+###### Enter Co-ordinate for y1: 4
+###### Enter Co-ordinate for y2: 4
+###### Distance between points (2, 4) and (4, 4) is 2
+
+
+print('-----------------------------------------Distance between two lines----------------------------------------------')
+
+# getting input from user
+
+x1 = int(input('Enter Co-ordinate for x1 : '))
+x2 = int(input('Enter Co-ordinate for x2 : '))
+y1 = int(input('Enter Co-ordinate for y1 : '))
+y2 = int(input('Enter Co-ordinate for y2 : '))
+
+#by putting in formulae
+
+dis_1 = (x2 - x1)**2
+dis_2 = (y2 - y1)**2
+
+# Output
+
+result = (dis_1 + dis_2) ** 1/2
+
+print(result) 
